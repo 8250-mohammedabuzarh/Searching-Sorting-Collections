13290ba825c3dfac66a2fe2051573d8f8bb14c51
Mohammed Abuzarh <Mohammed.Abuzarh@godigit.com>
2024-07-27 17:14:37 +0530

    smallest element that is greater than or equal to target value

---
+++ BinarySearch/SmallestEle.java
@@ -0,0 +1,31 @@
+package BinarySearch;
+
+public class SmallestEle {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 4, 6, 8, 9};
+        int target = 8;
+        int result = BinarySearch(arr, target);
+        if (result != -1) {
+            System.out.println("Smallest element that is greater than or equal to a target value at index:" + (result+1));
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+    public static int BinarySearch(int[] arr,int target)
+    {
+        int start = 0;
+        int end = arr.length-1;
+        for (int i = start; i <=end; i--) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return 0;
+    }
+}
